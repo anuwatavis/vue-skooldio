d8dc4ed0926608bafbf5e34bf9be4345a1c54f89
anuwatavis <anuwat.avis@gmail.com>
2020-05-06 13:45:49 +0700

    hello world

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">{{ text + "Anuwat" }} {{ number + 10 }}</div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  data() {
+    return {
+      text: "Hello World",
+      number: 3
+    };
+  }
+};
+</script>
